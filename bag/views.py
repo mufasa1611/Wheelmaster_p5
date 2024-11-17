f35d8b3a81d191082eca01c29444eb870e208e30
Mohamed <mufasa1611@gmail.com>
2024-11-17 22:58:22 +0100

    update context processor

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
-
 def view_bag(request):
     # A view that renders the bag contents page #
 
@@ -10,16 +9,15 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     # Add a quantity of the specified product to the shopping bag #
 
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    bag = request.session.get('bag', {})
+     quantity = int(request.POST.get('quantity'))
+     redirect_url = request.POST.get('redirect_url')
+     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
+     if item_id in list(bag.keys()):
         bag[item_id] += quantity
-    else:
+     else:
         bag[item_id] = quantity
 
-    request.session['bag'] = bag
-    print(request.session['bag'])
-    return redirect(redirect_url)
+     request.session['bag'] = bag
+     return redirect(redirect_url)
     
--- wheelbarrow/settings.py
+++ wheelbarrow/settings.py
@@ -80,6 +80,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -159,6 +160,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+FREE_DELIVERY_THRESHOLD = 1000000
+STANDARD_DELIVERY_PERCENTAGE = 10
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
