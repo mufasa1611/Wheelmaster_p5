a797f715ab50887280f44b98c2ac3dca6463434b
Mohamed <mufasa1611@gmail.com>
2024-11-17 16:47:03 +0100

    Add extra sort option to product page

--- products/views.py
+++ products/views.py
@@ -13,7 +13,6 @@ def all_products(request):
     categories = None
     sort = None
     direction = None
-
     if request.GET:
         # Sorting
         if 'sort' in request.GET:
@@ -23,9 +22,9 @@ def all_products(request):
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
             elif sortkey == 'category':
-                sortkey = 'category__name'
+                 sortkey = 'category__name'
             elif sortkey == 'price':
-                sortkey = 'price'
+                 sortkey = 'price'
             elif sortkey == 'rate':
                  products = products.annotate(rate=F('rating'))
                  sortkey = 'rate'
@@ -49,18 +48,14 @@ def all_products(request):
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-
     current_sorting = f'{sort}_{direction}'
-
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
         'current_sorting': current_sorting,
     }
-
     return render(request, 'products/products.html', context)
-
 
 def product_detail(request, product_id):
     """ A view to show individual product details """
@@ -70,5 +65,4 @@ def product_detail(request, product_id):
     context = {
         'product': product,
     }
-
     return render(request, 'products/product_detail.html', context)
