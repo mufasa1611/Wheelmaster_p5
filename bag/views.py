e69809371b260b5d6e86cad872bc80df7cdf8e29
Mohamed <mufasa1611@gmail.com>
2024-11-24 15:05:57 +0100

    - Modified bag, product detail, and products templates.Updated bag URLs and views. Added bag.js, main-nav.js, and quantity.js. Removed 'remove' button; setting quantity to 0 now removes the order.

--- bag/views.py
+++ bag/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
-# A view that renders the bag contents page #
+# Create your views here.
+
 def view_bag(request):
+    """ A view that renders the bag contents page """
+
     return render(request, 'bag/bag.html')
 
-    # Add a size and quantity of the specified product to the shopping bag #
 def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -29,3 +33,35 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+    
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+            
+    
+    try:
+        request.session['bag'] = bag
+        return redirect(reverse('view_bag'))
+    except Exception as e:
+        return HttpResponse(status=500)
+     
+          
