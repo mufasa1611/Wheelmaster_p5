db445f57a91e34597054957aecde02f794a11acb
Mohamed <mufasa1611@gmail.com>
2024-12-15 00:00:36 +0100

    Modify context, base.css

--- bag/contexts.py
+++ bag/contexts.py
@@ -6,14 +6,14 @@ from products.models import Product
 def bag_contents(request):
 
     bag_items = []
-    total = Decimal('0.00')
+    total = 0
     product_count = 0
     bag = request.session.get('bag', {})
 
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
-            total += Decimal(item_data) * product.price
+            total += item_data * product.price
             product_count += item_data
             bag_items.append({
                 'item_id': item_id,
@@ -23,7 +23,7 @@ def bag_contents(request):
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
-                total += Decimal(quantity) * product.price
+                total += quantity * product.price
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
@@ -32,22 +32,22 @@ def bag_contents(request):
                     'size': size,
                 })
 
-    if total < Decimal(settings.FREE_DELIVERY_THRESHOLD):  # Cast threshold to Decimal
-        delivery = total * (Decimal(settings.STANDARD_DELIVERY_PERCENTAGE) / Decimal('100'))
-        free_delivery_delta = Decimal(settings.FREE_DELIVERY_THRESHOLD) - total
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
-        delivery = Decimal('0.00')
-        free_delivery_delta = Decimal('0.00')
+        delivery = 0
+        free_delivery_delta = 0
     
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
-        'free_delivery_threshold': Decimal(settings.FREE_DELIVERY_THRESHOLD),
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
 
