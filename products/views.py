781c28dc272e042256ccdaaa7528a5962a6f56bf
Mohamed <mufasa1611@gmail.com>
2024-12-26 17:24:51 +0100

    Added product delete function, url

--- products/views.py
+++ products/views.py
@@ -95,7 +95,7 @@ def add_product(request):
 
     return render(request, template, context)
 
-
+@login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
     product = get_object_or_404(Product, pk=product_id)
@@ -118,3 +118,15 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+@login_required
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
