c122509a62b6aa261c165e3a747144a331716b12
Mohamed <mufasa1611@gmail.com>
2024-12-28 15:00:40 +0100

    Add rating value condition in products form

--- products/forms.py
+++ products/forms.py
@@ -8,11 +8,15 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
-
+    def clean_rating(self):
+        rating = self.cleaned_data['rating']
+        if rating < 0 or rating > 5:
+           raise forms.ValidationError('Invalid rating. Please enter a rating between 0 and 5.')
+        return rating
     image = forms.ImageField(label='Image',
                              required=False,
                              widget=CustomClearableFileInput)
-
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
