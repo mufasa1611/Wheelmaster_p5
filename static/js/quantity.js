b6b6ec92466eee6a389e0ea8dc60ffbddfa8e504
Mohamed <mufasa1611@gmail.com>
2024-12-29 02:20:27 +0100

    Updated CSS and js file

--- static/js/quantity.js
+++ static/js/quantity.js
@@ -1,45 +1,44 @@
 // Disable +/- buttons outside 1-99 range
 function handleEnableDisable(itemId) {
     var currentValue = parseInt($(`#id_qty_${itemId}`).val());
-    var minusDisabled = currentValue <= 1;
-    var plusDisabled = currentValue >= 99;
+    var minusDisabled = currentValue < 2; // Disable if less than 2
+    var plusDisabled = currentValue > 98; // Disable if more than 98
     $(`#decrement-qty_${itemId}`).prop('disabled', minusDisabled);
     $(`#increment-qty_${itemId}`).prop('disabled', plusDisabled);
 }
 
 // Ensure proper enabling/disabling of all inputs on page load
-var allQtyInputs = $('.qty_input');
-for(var i = 0; i < allQtyInputs.length; i++){
-    var itemId = $(allQtyInputs[i]).data('item_id');
+$('.qty_input').each(function () {
+    var itemId = $(this).data('item_id');
     handleEnableDisable(itemId);
-}
+});
 
 // Check enable/disable every time the input is changed
-$('.qty_input').change(function() {
+$('.qty_input').change(function () {
     var itemId = $(this).data('item_id');
     handleEnableDisable(itemId);
 });
 
 // Increment quantity
-$('.increment-qty').click(function(e) {
-   e.preventDefault();
-   var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
-   var currentValue = parseInt($(closestInput).val());
-   $(closestInput).val(currentValue + 1);
-   var itemId = $(this).data('item_id');
-   handleEnableDisable(itemId);
+$('.increment-qty').click(function (e) {
+    e.preventDefault();
+    var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
+    var currentValue = parseInt($(closestInput).val());
+    $(closestInput).val(currentValue + 1);
+    var itemId = $(this).data('item_id');
+    handleEnableDisable(itemId);
 });
 
 // Decrement quantity
-$('.decrement-qty').click(function(e) {
-   e.preventDefault();
-   var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
-   var currentValue = parseInt($(closestInput).val());
-   
-   // Prevent going below 1
-   if (currentValue > 1) {
-       $(closestInput).val(currentValue - 1);
-       var itemId = $(this).data('item_id');
-       handleEnableDisable(itemId);
-   }
+$('.decrement-qty').click(function (e) {
+    e.preventDefault();
+    var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
+    var currentValue = parseInt($(closestInput).val());
+    
+    // Prevent going below 1
+    if (currentValue > 1) {
+        $(closestInput).val(currentValue - 1);
+        var itemId = $(this).data('item_id');
+        handleEnableDisable(itemId);
+    }
 });
