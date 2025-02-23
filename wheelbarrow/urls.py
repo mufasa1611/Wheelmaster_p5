d5cea2c4b76c9a0bef41d8293eddedb3a04c5207
mufasa1611 <mufasa1611@gmail.com>
2025-02-23 17:10:11 +0100

    Cleanup: Remove whitespace and add custom error handlers

--- wheelbarrow/urls.py
+++ wheelbarrow/urls.py
@@ -28,7 +28,6 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler403 = custom_403
