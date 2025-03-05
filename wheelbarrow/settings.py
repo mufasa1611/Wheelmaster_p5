2e7a2b4ee39170e6225b455dab3c8e88d0776ace
mufasa1611 <mufasa1611@gmail.com>
2025-03-05 22:53:48 +0100

    Add categories_processor

--- wheelbarrow/settings.py
+++ wheelbarrow/settings.py
@@ -123,9 +123,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-             os.path.join(BASE_DIR, 'templates'),
-             os.path.join(BASE_DIR, 'templates', 'allauth'),
-            ],
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -135,6 +135,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
+                'products.context_processors.categories_processor',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
