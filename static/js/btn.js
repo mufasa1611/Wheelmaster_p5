84ac1f1c5edce814cc8116721cc76139d629e2a9
mufasa1611 <mufasa1611@gmail.com>
2025-03-10 23:09:58 +0100

    Add functions for go to top button

--- static/js/btn.js
+++ static/js/btn.js
@@ -1,11 +1,38 @@
-$('#new-image').change(function() {
+$('#new-image').change(function () {
     var file = $('#new-image')[0].files[0];
     $('#filename').text(`Image will be set to: ${file.name}`);
 });
 
-$('.btn-btt').click(function(e) {
+$(document).ready(function () {
+    // Initially hide the button
+    $('.btn-btt').hide();
+
+    // Show/hide the button based on scroll position
+    $(window).scroll(function () {
+        var scrollPosition = $(this).scrollTop();
+        var footerOffset = $('footer').offset().top; // Get the footer position
+        var windowHeight = $(window).height(); // Get the height of the viewport
+
+
+        // Show button when the bottom of the viewport is within 50 pixels of the footer
+        if (scrollPosition + windowHeight >= footerOffset - 50) {
+            $('.btn-btt').fadeIn();
+
+        }
+        // Hide button when at the top of the page or above the footer
+        else if (scrollPosition < 20) {
+            $('.btn-btt').fadeOut();
+
+        }
+    });
+});
+
+$('.btn-btt').click(function (e) {
     e.preventDefault();
+
     $('html, body').animate({
         scrollTop: 0
-    }, 1500);
+    }, 1500, function () {
+
+    });
 });
